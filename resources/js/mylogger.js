3646189bca3441f5e6749a4886812220fbc82f5b
Ben Smith Lea <carbontax@gmail.com>
2012-10-14 22:16:26 -0400

    change to browser console appender

--- resources/js/mylogger.js
+++ resources/js/mylogger.js
@@ -1,7 +1,10 @@
 var log = log4javascript.getLogger();
 
-var popUpAppender = new log4javascript.PopUpAppender();
-log.addAppender(popUpAppender);
+/*var popUpAppender = new log4javascript.PopUpAppender();
+log.addAppender(popUpAppender);*/
 
 /*var ajaxAppender = new log4javascript.AjaxAppender("/index.html");
 log.addAppender(ajaxAppender);*/
+
+var browserConsoleAppender = new log4javascript.BrowserConsoleAppender();
+log.addAppender(browserConsoleAppender);
