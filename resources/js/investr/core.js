20814a6066f8337e84761be638d0adcc009e8eb8
Ben Smith Lea <carbontax@gmail.com>
2012-10-14 22:16:57 -0400

    refactored polling

--- resources/js/investr/core.js
+++ resources/js/investr/core.js
@@ -1,18 +1,16 @@
+/*jslint todo: true, white: true */
+/*global User: false, Game: false */
 function InvestrViewModel() {
+	"use strict";
 	var self = this;
-	self.clearMessages = function() {
-		// TODO remove in favour of bootstrapGrowl
-		$('#messages').empty()
-		.removeClass("alert")
-		.removeClass("alert-success")
-		.removeClass("alert-error");
-	}
+	var stopPollingUserFlag = false;
+	var stopPollingGameFlag = false;
 
 	self.title = "Investr";
 
 	self.user = ko.observable(new User());
-	self.username = ko.computed(function() {
-		if ( self.user() && self.user().username ) {
+	self.username = ko.computed(function () {
+		if (self.user() && self.user().username) {
 			return self.user().username;
 		}
 		return "";
@@ -20,20 +18,20 @@ function InvestrViewModel() {
 	
 	self.showLoginForm = ko.observable(false);
 	
-	self.loggedIn = ko.computed(function() {
-		if ( self.username() === "" ) {
+	self.loggedIn = ko.computed(function () {
+		if (self.username() === "") {
 			return false;
 		}
 		return true;
 	});
-	self.notLoggedIn = ko.computed(function() {
+	self.notLoggedIn = ko.computed(function () {
 		return ! self.loggedIn();
 	});
 	self.enableLoginButton = ko.observable(true);
 
 	self.game = ko.observable();
-	self.gameTitle = ko.computed(function() {
-		if ( self.game() ) {
+	self.gameTitle = ko.computed(function () {
+		if (self.game()) {
 			return self.title + "." + self.game().id + " Year " + self.game().year();
 		}
 		return self.title;
@@ -41,19 +39,18 @@ function InvestrViewModel() {
 
 	self.newGame = ko.observable();
 
-	self.shouldDisplayGamesLists = ko.computed(function() {
-		if ( self.notLoggedIn() || self.game() ) {
+	self.shouldDisplayGamesLists = ko.computed(function () {
+		if (self.notLoggedIn() || self.game()) {
 			return false;
 		}
 		return true;
 	});
 
-	self.ajaxFailureCallback = function(xhr) {
-			self.clearMessages();
-			if ( xhr.status === 401 ) {
-				self.user(new User());
-			}
-			$('#messages').addClass("alert alert-error").append(xhr.responseText);
+	self.ajaxFailureCallback = function (xhr) {
+		if (xhr.status === 401) {
+			self.user(new User());
+		}
+		$('#messages').addClass("alert alert-error").append(xhr.responseText);
 	};
 
 	function resetLoginForm() {
@@ -63,26 +60,191 @@ function InvestrViewModel() {
 		self.enableLoginButton(true);
 	}
 
-	self.submitLoginForm = function() {
-		self.clearMessages();
+	self.getNewGame = function () {
+		// TODO this is unused 
+		// get game defaults from server
+		$.ajax({
+			url: '/investr-game/api/games/new',
+			type: 'get',
+			dataType: 'json',
+			success: function (data) {
+				self.newGame(data);
+				$('#new-game-form-dialog').modal('toggle');
+			},
+			error: self.ajaxFailureCallback
+		});
+	};
+
+	self.cancelNewGame = function () {
+		// unused
+		self.newGame(null);
+		$('#new-game-form-dialog').modal('toggle');
+	};
+
+	self.saveNewGame = function () {
+		// unused
+		$.ajax({
+			preventDefault: true,
+			url: '/investr-game/api/games',
+			type: 'post',
+			data: self.newGame(),
+			dataType: 'json',
+			success: function (data) {
+				self.user().newGames.push(new Game(data));
+				$('#new-game-form-dialog').modal('toggle');
+			},
+			error: function (xhr) {
+				$('#new-game-form-dialog').modal('toggle');
+				self.ajaxFailureCallback(xhr);
+			}
+		});		
+	};
+
+	self.mayJoinGame = function (game) {
+		return game.playerCount() < game.number_of_players();
+	};
+
+	self.joinGame = function (game) {
+		$.ajax({
+			url: '/investr-game/api/games/' + game.id + '/join',
+			type: 'post',
+			dataType: 'json',
+			success: function (data) {
+				// TODO use ko.observableArray()
+				window.location.reload();
+			},
+			error: self.ajaxFailureCallback
+		});				
+	};
+
+	self.openGame = function (game) {
+		$.ajax({
+			url: '/investr-game/api/games/' + game.id,
+			dataType: 'json',
+			type: 'get',
+			success: function (data) {
+				self.game(new Game(data));
+				startPollingGame();
+			},
+			error: self.ajaxFailureCallback
+		});
+	};
+	
+	self.logoutAction = function () {
+		stopAllPolling();
+		self.user(new User());
+		self.game(null);
+		$.ajax({
+			url: '/investr-game/api/logout',
+			type: 'post',
+			success: function () {
+				resetLoginForm();
+				self.showLoginForm(true);
+			}
+		});
+	};
+	
+	self.showSpinner = ko.observable(false);
+	
+	// GAME POLLING
+	var startPollingGame = function () {
+		stopPollingGameFlag = false;
+		stopPollingUserFlag = true;
+		pollGame();
+	};
+
+	self.pollGameDelay = ko.observable(30000);
+	var pollGame = function () {
+		if (stopPollingGameFlag) {
+			return false;
+		}
+		var pollString = self.game().getPollString();
+		log.debug("pollGame: " + pollString);
+		$.ajax({
+			url: '/investr-game/api/games/' + self.game().id + '/poll',
+			type: 'post',
+			data: {'gs': pollString},
+			dataType: 'json',
+			success: function (data) {
+				if (data) {
+					self.game().loadGame(data);
+				}
+			},
+			error: function (xhr) {
+				log.debug("ERROR POLLING GAME");
+			},
+			complete: function () {
+				setTimeout(pollGame, self.pollGameDelay());
+			},
+			timeout: 30000
+		});
+	};
+
+	// USER POLLING
+	var startPollingUser = function () {
+		stopPollingUserFlag = false;
+		stopPollingGameFlag = true;
+		pollUser();
+	};
+	
+	self.pollUserDelay = ko.observable(30000);
+	var pollUser = function () {
+		if (stopPollingUserFlag) {
+			return false;
+		}
+		var pollString = self.user().getGamesPollString();
+		log.debug("pollUser: " + pollString);
+		$.ajax({
+			url: '/investr-game/api/users/poll',
+			type: 'post',
+			data: {'gs': pollString},
+			dataType: 'json',
+			success: function (data) {
+				if (data) {
+					self.user().loadData(data);
+				}
+			},
+			error: function (xhr) {
+				stopPollingUserFlag = true;
+				self.showLoginForm(true);
+				self.ajaxFailureCallback(xhr);
+			},
+			complete: function () {
+				setTimeout(pollUser, self.pollUserDelay());
+			},
+			timeout: 30000
+		});
+	};
+	
+	var stopAllPolling = function () {
+		stopPollingGameFlag = true;
+		stopPollingUserFlag = true;
+	};
+	
+	self.viewAllGames = function () {
+		self.game(null);
+		startPollingUser();
+	};
+
+	self.submitLoginForm = function () {
 		self.enableLoginButton(false);
 		self.showSpinner(true);
 		var data = {
 			email: $('#email', '#login-form').val(),
 			password: $('#password', '#login-form').val()
-		}
+		};
 		$.ajax({
 			url: '/investr-game/api/login',
 			data: data,
 			dataType: 'json',
 			type: 'post',
-			success: function(data) {
+			success: function (data) {
 				self.showLoginForm(false);
 				resetLoginForm();
 				self.user().loadData(data);
 				startPollingUser();
 			},
-			error: function(xhr) {
+			error: function (xhr) {
 				self.showSpinner(false);
 				self.enableLoginButton(true);
 //				self.ajaxFailureCallback(xhr);
@@ -93,174 +255,6 @@ function InvestrViewModel() {
 			}
 		});
 		return false;
-	};
-
-	self.getNewGame = function() {
-		// TODO this is unused 
-		// get game defaults from server
-		$.ajax({
-			url: '/investr-game/api/games/new',
-			type: 'get',
-			dataType: 'json',
-			success: function(data) {
-				self.newGame(data);
-				$('#new-game-form-dialog').modal('toggle');
-			},
-			error: self.ajaxFailureCallback
-		});
-	};
-
-	self.cancelNewGame = function() {
-		// unused
-		self.newGame(null);
-		$('#new-game-form-dialog').modal('toggle');
-	}
-
-	self.saveNewGame = function() {
-		// unused
-		$.ajax({
-			preventDefault: true,
-			url: '/investr-game/api/games',
-			type: 'post',
-			data: self.newGame(),
-			dataType: 'json',
-			success: function(data) {
-				self.user().newGames.push(new Game(data));
-				$('#new-game-form-dialog').modal('toggle');
-			},
-			error: function(xhr) {
-				$('#new-game-form-dialog').modal('toggle');
-				self.ajaxFailureCallback(xhr);
-			}
-		});		
-	}
-
-	self.mayJoinGame = function(game) {
-		return game.playerCount() < game.number_of_players();
-	}
-
-	self.joinGame = function(game) {
-		self.clearMessages();
-		$.ajax({
-			url: '/investr-game/api/games/' + game.id + '/join',
-			type: 'post',
-			dataType: 'json',
-			success: function(data) {
-				// TODO use ko.observableArray()
-				window.location.reload();
-			},
-			error: self.ajaxFailureCallback
-		});				
-	}
-
-	self.openGame = function(game) {
-		self.clearMessages();
-		$.ajax({
-			url: '/investr-game/api/games/' + game.id,
-			dataType: 'json',
-			type: 'get',
-			success: function(data) {
-				stopPollingUserFlag = true;
-				self.game(new Game(data));
-				self.stopPollingGame = false;
-				self.pollGame();
-			},
-			error: self.ajaxFailureCallback
-		});
-	}
-	
-	self.viewAllGames = function() {
-		self.stopPollingGame = true;
-		self.game(null);
-		startPollingUser();
-	}
-
-	self.logoutAction = function() {
-		self.stopPollingGame = true;
-		stopPollingUserFlag = true;
-		self.user(new User());
-		self.game(null);
-		$.ajax({
-			url: '/investr-game/api/logout',
-			type: 'post',
-			success: function() {
-				resetLoginForm();
-				self.showLoginForm(true);
-			}
-		});
-	}
-	
-	self.showSpinner = ko.observable(false);
-	
-	// GAME POLLING
-	self.stopPollingGame = false;
-	self.pollGameDelay = ko.observable(30000);
-	self.pollGame = function() {
-		if ( self.stopPollingGame ) {
-			return false;
-		}
-		var pollString = self.game().getPollString();
-		console.log("pollGame: " + pollString);
-		$.ajax({
-			url: '/investr-game/api/games/' + self.game().id + '/poll',
-			type: 'post',
-			data: {'gs': pollString},
-			dataType: 'json',
-			success: function(data) {
-				if ( data ) {
-					self.game().loadGame(data);
-				}
-			},
-			error: function(xhr) {
-				window.console && console.log("ERROR POLLING GAME");
-			},
-			complete: function() {
-				setTimeout(
-					self.pollGame,
-					self.pollGameDelay()
-				);
-			},
-			timeout: 30000
-		});
-	};
-
-	// USER POLLING
-	startPollingUser = function() {
-		stopPollingUserFlag = false;
-		pollUser();
-	};
-	
-	var stopPollingUserFlag = false;
-	self.pollUserDelay = ko.observable(30000);
-	pollUser = function() {
-		if ( stopPollingUserFlag ) {
-			return false;
-		}
-		var pollString = self.user().getGamesPollString();
-		console.log("pollUser: " + pollString);
-		$.ajax({
-			url: '/investr-game/api/users/poll',
-			type: 'post',
-			data: {'gs': pollString},
-			dataType: 'json',
-			success: function(data) {
-				if ( data ) {
-					self.user().loadData(data);
-				}
-			},
-			error: function(xhr) {
-				stopPollingUserFlag = true;
-				self.showLoginForm(true);
-				self.ajaxFailureCallback(xhr);
-			},
-			complete: function() {
-				setTimeout(
-					pollUser,
-					self.pollUserDelay()
-				);
-			},
-			timeout: 30000
-		});
 	};
 	
 	// START POLLING ON PAGE LOAD
