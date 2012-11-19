55d823b8022ee4a555b4617542cec90ddfb8fca6
Ben Smith Lea <carbontax@gmail.com>
2012-11-18 21:21:08 -0500

    Grunt used to create a tgz distribution file. Html file header is rewritten (with grunt-targethtml) depending on target

---
+++ README.md
@@ -0,0 +1,7 @@
+# Investr Game
+
+A board game style online investment simulation for multiple players. 
+This turn based game contrasts two approaches to investing: 
+
+1. dividend-chasing
+1. risk taking. 
--- grunt.js
+++ grunt.js
@@ -1,5 +1,7 @@
 /*global module:false*/
 module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-targethtml');
+	grunt.loadNpmTasks('grunt-contrib');
 
   // Project configuration.
   grunt.initConfig({
@@ -21,13 +23,53 @@ module.exports = function(grunt) {
       dist: {
 //          src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
         src: ['<banner:meta.banner>', 'resources/js/investr/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+        dest: 'dist/resources/js/<%= pkg.name %>.js'
+      }
+    },
+    clean: {
+      dist: "dist"
+    },
+    copy: {
+      dist: {
+        files: {
+            "dist/api/": [
+              "api/admin/**",
+              "api/controllers/**",
+              "api/db/**",
+              "api/etc/config.ini.sample",
+              "api/lib/**",
+              "api/models/**",
+              "api/index.php",
+              "api/sample.htaccess"],
+            "dist/epiphany/src/": "epiphany/src/**",
+            "dist/resources/bootstrap/js/": "resources/bootstrap/js/bootstrap.min.js",
+            "dist/resources/bootstrap/css/": "resources/bootstrap/css/*.min.css",
+            "dist/resources/bootstrap/img/": "resources/bootstrap/img/*",
+            "dist/resources/bootstrap-growl/": "resources/bootstrap-growl/*.min.js",
+            "dist/resources/js/": [
+              "resources/js/accounting.js",
+              "resources/js/bootbox.min.js", 
+              "resources/js/jquery.1.7.2.min.js", 
+              "resources/js/jquery.validate.min.js",
+              "resources/js/json2.js",
+              "resources/js/knockout-2.1.0.js", 
+              "resources/js/log4javascript.js", 
+              "resources/js/mylogger.js"],
+            "dist/resources/images/": "resources/images/*"
+        }
+      }
+    },
+    less: {
+      'default': {
+        files: {
+         "dist/resources/css/<%= pkg.name %>.css": "resources/css/investr.less"
+        }
       }
     },
     min: {
       dist: {
         src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        dest: 'dist/resources/js/<%= pkg.name %>.min.js'
       }
     },
     watch: {
@@ -57,10 +99,27 @@ module.exports = function(grunt) {
         bootbox: false
       }
     },
-    uglify: {}
+    targethtml: {
+      'default': {
+        src: 'index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    uglify: {},
+    compress: {
+      options: {
+        rootDir: "investr-game"
+      },
+      tgz: {
+        files: {
+        "<%= pkg.name %>-<%= pkg.version %>.tgz": "dist/**"
+        }
+      }
+    }
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('default', 'clean lint qunit concat min targethtml copy less compress');
+  //grunt.registerTask('clean', 'clean');
 
 };
